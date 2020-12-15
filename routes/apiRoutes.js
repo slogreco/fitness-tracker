49aac8692006179d2a4bb4cb26b975e419060e1d
Slogreco <shoshwright@hotmail.com>
2020-12-15 12:47:02 -0700

    added api routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,48 @@
+const router = require("express").Router();
+const { isValidObjectID } = require("mongoose");
+const { workout } = require("../models");
+const db = require("../models");
+
+// create a new workout
+router.post("/api/workouts", (req, res) => {
+    db.workout.create(req.body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+
+// get all workouts
+router.get("/api/workouts", (req, res) => {
+    db.workout.findAll({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+
+// update workout
+router.put("/api/workouts/:id", (req, res) => {
+    const { body, params } = req;
+
+    db.workout.findByIdAndUpdate(params.id, {
+        $push: { exercises: body }
+    })
+    .then((dbWorkout) => {
+        res.json(dbWorkout);
+    })
+    .catch((err) => {
+        res.status(400).json(err);
+        console.log(err);
+    });
+});
+
+
+
+module.exports = router;
